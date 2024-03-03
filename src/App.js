35b98c145f63c42bf174008a18626f1e2880e950
haeun <hahi1108@gmail.com>
2024-03-03 14:23:32 +0900

    feat : 일기 등록/삭제/수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    }
+    dataId.current += 1;
+    setData([newItem,...data]);
+  };
+
+  const onRemove = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다.`);
+    const newDiaryList = data.filter((it)=>it.id !== targetId)
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    console.log(`${targetId}가 수정되었습니다.`);
+    setData(
+      data.map((it) => it.id === targetId
+        ? {...it,content:newContent} : it
+      )
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DiaryEditor onCreate={onCreate}/>
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}/>
     </div>
   );
 }
